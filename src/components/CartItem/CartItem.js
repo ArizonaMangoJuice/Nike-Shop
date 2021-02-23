b9af514fdf89ef464ded5dc9b3ead085e5544118
AMJ <isaelwebdeveloper@gmail.com>
2021-02-23 15:35:07 -0700

    added bg to the shoes

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,10 +1,10 @@
 import React, {useContext, useState} from 'react';
 import './CartItem.css';
 
-const CartItem = ({amount, title, image, price}) => {
+const CartItem = ({amount, title, image, price, bg}) => {
     return (
         <div className='cart-item'>
-            <div className='image-container'>
+            <div className='image-container' style={{backgroundColor: bg}}>
                 <img src={image.default} />
             </div>
             <div className='text-container'>
